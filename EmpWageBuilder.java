a358a3bc95def7f7f8d92c5c744f9637420cc85b
Debojyoti Mukherjee <59368291+debojyotimukh@users.noreply.github.com>
2020-09-27 13:47:35 +0530

    max working hrs condition implemented

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -3,12 +3,14 @@ public class EmpWageBuilder {
     public static final int IS_FULL_TIME = 2;
     public static final int IS_PART_TIME = 1;
     public static final int WORKING_DAY_MONTH = 20;
+    public static final int MAX_WORKING_HRS = 100;
 
     public static void main(String[] args) {
         System.out.println("Welcome to Employee wage computation program");
 
         int totalWage = 0;
-        for (int day=0; day < WORKING_DAY_MONTH; day++) {
+        int totalHrs = 0;
+        for (int day = 0; day < WORKING_DAY_MONTH && totalHrs < MAX_WORKING_HRS; day++) {
             int empHrs = 0;
             int empWage = 0;
             int empCheck = (int) Math.floor(Math.random() * 10) % 3;
@@ -22,7 +24,7 @@ public class EmpWageBuilder {
                 default:
                     empHrs = 0;
             }
-
+            totalHrs += empHrs;
             empWage = empHrs * EMP_RATE_PER_HOUR;
             totalWage += empWage;
         }
