6718954e4f2c37f749b6d3aaef3833e5e8382653
Debojyoti Mukherjee <59368291+debojyotimukh@users.noreply.github.com>
2020-09-27 13:56:27 +0530

    class method calculateWage() implemented

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -1,13 +1,11 @@
-public class EmpWageBuilder {
+class Employee{
     public static final int EMP_RATE_PER_HOUR = 20;
     public static final int IS_FULL_TIME = 2;
     public static final int IS_PART_TIME = 1;
     public static final int WORKING_DAY_MONTH = 20;
     public static final int MAX_WORKING_HRS = 100;
 
-    public static void main(String[] args) {
-        System.out.println("Welcome to Employee wage computation program");
-
+    public int calculateWage(){
         int totalWage = 0;
         int totalHrs = 0;
         for (int day = 0; day < WORKING_DAY_MONTH && totalHrs < MAX_WORKING_HRS; day++) {
@@ -28,6 +26,15 @@ public class EmpWageBuilder {
             empWage = empHrs * EMP_RATE_PER_HOUR;
             totalWage += empWage;
         }
-        System.out.println("Emp Wage: " + totalWage);
+        return totalWage;
+    }
+}
+public class EmpWageBuilder {
+    
+    public static void main(String[] args) {
+        System.out.println("Welcome to Employee wage computation program");
+
+        Employee e1=new Employee();
+        System.out.println("Emp Wage: " + e1.calculateWage());
     }
 }
