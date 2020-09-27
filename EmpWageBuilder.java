c1b8baf49f1658c301722651cbd2858b08338f00
Debojyoti Mukherjee <59368291+debojyotimukh@users.noreply.github.com>
2020-09-27 13:35:20 +0530

    switch case implemented

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -1,16 +1,25 @@
 public class EmpWageBuilder {
+    public static final int EMP_RATE_PER_HOUR = 20;
+    public static final int IS_FULL_TIME = 2;
+    public static final int IS_PART_TIME = 1;
+
     public static void main(String[] args) {
         System.out.println("Welcome to Employee wage computation program");
 
-        final int IS_FULL_TIME = 1;
-        final int EMP_RATE_PER_HOUR = 20;
         int empHrs = 0;
         int empWage = 0;
-        int empCheck = (int) Math.floor(Math.random() * 10) % 2;
-        if (empCheck == IS_FULL_TIME)
-            empHrs = 8;
-        else
-            empHrs = 4;
+        int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+        switch (empCheck) {
+            case IS_FULL_TIME:
+                empHrs = 8;
+                break;
+            case IS_PART_TIME:
+                empHrs = 4;
+                break;
+            default:
+                empHrs = 0;
+        }
+
         empWage = empHrs * EMP_RATE_PER_HOUR;
         System.out.println("Emp Wage: " + empWage);
     }
