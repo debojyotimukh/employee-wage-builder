399ed8548023f306b53c919f5343e3f1f816351a
Debojyoti Mukherjee <59368291+debojyotimukh@users.noreply.github.com>
2020-09-27 13:23:08 +0530

    absent or present implemented

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -1,5 +1,13 @@
-public class EmpWageBuilder{
+public class EmpWageBuilder {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee wage computation program");
+
+        int IS_FULL_TIME = 1;
+        int empCheck = (int) Math.floor(Math.random() * 10) % 2;
+
+        if (empCheck == IS_FULL_TIME)
+            System.out.println("Employee is present");
+        else
+            System.out.println("Employee is absent");
     }
 }
