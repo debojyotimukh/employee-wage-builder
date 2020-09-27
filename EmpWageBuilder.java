411365ed7dc6c456c2eed37934210a61dfb93ea1
Debojyoti Mukherjee <59368291+debojyotimukh@users.noreply.github.com>
2020-09-27 13:30:05 +0530

    implemented part time case

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -6,9 +6,11 @@ public class EmpWageBuilder {
         final int EMP_RATE_PER_HOUR = 20;
         int empHrs = 0;
         int empWage = 0;
-        double empCheck = Math.floor(Math.random() * 10) % 2;
+        int empCheck = (int) Math.floor(Math.random() * 10) % 2;
         if (empCheck == IS_FULL_TIME)
             empHrs = 8;
+        else
+            empHrs = 4;
         empWage = empHrs * EMP_RATE_PER_HOUR;
         System.out.println("Emp Wage: " + empWage);
     }
